c5125261687ef74a25584213bd55fc8c194fd756
seven332 <seven332@163.com>
2023-10-02 12:04:36 +0800

    feat: wasi handle json params

--- wasi/src/main.cpp
+++ wasi/src/main.cpp
@@ -2,7 +2,34 @@
 #include <cstdint>
 #include <iostream>
 #include <istream>
+#include <nlohmann/json.hpp>
 #include <string>
+
+#pragma mark - Update
+
+struct UpdateParams {
+    std::string file;
+    std::string content;
+    std::string kind;
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(UpdateParams, file, content, kind)
+};
+
+static void Update(const UpdateParams& params) {
+    // TODO:
+    std::cout << "update: " << params.file << std::endl;
+}
+
+#pragma mark - Close
+
+struct CloseParams {
+    std::string file;
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CloseParams, file)
+};
+
+static void Close(const CloseParams& params) {
+    // TODO:
+    std::cout << "Close: " << params.file << std::endl;
+}
 
 static std::string GetLine() {
     constexpr std::uint32_t kNextLine = 10;
@@ -30,17 +57,24 @@ static constexpr std::uint32_t Hash(const char* str) {
 
 extern "C" {
 
+#define CALL(Action)                    \
+auto j = nlohmann::json::parse(params); \
+auto params = j.get<Action##Params>();  \
+Action(params);
+
 int main(void) {
     while (true) {
         auto method = GetLine();
         auto params = GetLine();
         switch (Hash(method.data())) {
-        case Hash("sksl/update"):
-            // TODO:
+        case Hash("sksl/update"): {
+            CALL(Update)
             break;
-        case Hash("sksl/close"):
-            // TODO:
+        }
+        case Hash("sksl/close"): {
+            CALL(Close)
             break;
+        }
         }
     }
     return 0;
