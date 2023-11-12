47ea0224f2ce9fec8c5221b017dd7333c385bfe9
seven332 <seven332@163.com>
2023-11-12 19:57:29 +0800

    fix: wasm golden test

--- wasm/golden/golden_test.cpp
+++ wasm/golden/golden_test.cpp
@@ -30,21 +30,21 @@ static std::string_view Substr(std::string_view content, const SkSLRange& range)
 }
 
 void ExpectTokens(const Module& module, const std::vector<ExpectedToken>& expected) {
-    auto i1 = module.tokens.begin();
+    auto i1 = module.GetTokens().begin();
     auto i2 = expected.begin();
-    for (; i1 != module.tokens.end() && i2 != expected.end(); ++i1, ++i2) {
+    for (; i1 != module.GetTokens().end() && i2 != expected.end(); ++i1, ++i2) {
         ASSERT_EQ(i1->range, i2->range);
         ASSERT_EQ(i1->is_reference, i2->is_reference);
-        ASSERT_EQ(Substr(module.content, i1->range), i2->name);
+        ASSERT_EQ(Substr(module.GetContent(), i1->range), i2->name);
     }
-    ASSERT_EQ(module.tokens.size(), expected.size());
+    ASSERT_EQ(module.GetTokens().size(), expected.size());
 }
 
 std::string ToString(const Module& module) {
     std::stringstream ss;
-    for (const auto& token : module.tokens) {
+    for (const auto& token : module.GetTokens()) {
         ss << "{{" << token.range.start << ", " << token.range.end << "}, " << (token.is_reference ? "true" : "false")
-           << ", \"" << Substr(module.content, token.range) << "\"},\n";
+           << ", \"" << Substr(module.GetContent(), token.range) << "\"},\n";
     }
     return std::move(ss).str();
 }
